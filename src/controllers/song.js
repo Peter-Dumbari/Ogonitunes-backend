3687b1463a388496d72e13d7b4831c96f59dda76
Peter Dumbari <dummytwiz@gmail.com>
2026-03-31 11:24:05 +0100

    made some adjustment on music download

--- src/controllers/song.js
+++ src/controllers/song.js
@@ -3,6 +3,7 @@ import slugify from "slugify";
 import { Song } from "../models/song.js";
 import cloudinary from "../../config/cloudinary.js";
 import { Artist } from "../models/artist.js";
+import axios from "axios";
 
 export const upload = asyncHandler(async (req, res) => {
   const { title, artist, genre, year_release } = req.body;
@@ -146,14 +147,30 @@ export const updateSong = asyncHandler(async (req, res) => {
 export const downloadSong = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
-  const song = await Song.findById(id);
+  const song = await Song.findById(id).populate("artist", "name");
   if (!song)
     return res.status(404).json({ message: "Song not found", status: "error" });
+
+  const fileUrl = song.audio.url;
+
+  const filename =
+    `${song.title} - ${song.artist.name} | Ogonitunes.mp3`.replace(
+      /[^\w\d\s.-]/g,
+      "",
+    );
+
+  const response = await axios.get(fileUrl, {
+    responseType: "stream",
+  });
+
+  res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+
+  res.setHeader("Content-Type", "audio/mpeg");
 
   song.download_count = (song.download_count || 0) + 1;
   await song.save();
 
-  return res.status(200).redirect(song.audio.url);
+  response.data.pipe(res);
 });
 export const deleteSong = asyncHandler(async (req, res) => {
   const { id } = req.params;
